f4fc4fdd65200341cd96240ea1f3d2bc9b2561ea
azeloc <fe_poliano@yahoo.com.br>
2016-12-13 20:18:14 -0200

    fixed documentation issues

---
+++ R/tpur.R
@@ -0,0 +1,9 @@
+#' tpur: A package for acessing the Unified Processual Tables from the brazillian National Justice Council.
+#'
+#' 
+#' @section tpur functions:
+#' funcoes
+#'
+#' @docType package
+#' @name tpur
+NULL
