3631fdf8b0e826addd0cb056209aa92eb5f077ef
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-02 18:05:24 -0200

    adding globalVariables

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+globalVariables(c('.', 'V0', 'cod_pai', 'codigo', 'control_table', 
+                  'court', 'grupo', 'level', 'n', 'n1', 
+                  'n2', 'n3', 'n4', 'n5', 'n6',
+                  'nas', 'table_type', 'variavel', 'valor'))
