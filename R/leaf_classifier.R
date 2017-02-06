310876a82a668821720ff88fb4ecd6e37bbc2c84
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-06 01:28:15 -0200

    new leaf algo

--- R/leaf_classifier.R
+++ R/leaf_classifier.R
@@ -14,14 +14,27 @@ function (edges) {
 #' 
 #' @param pre_processed_table A Unified Processual Table processed with build_table.
 #' @return A list with the edges of the table.
-leaf_classifier <-
-function(pre_processed_table){
+leaf_classifier <- function(pre_processed_table){
    pre_processed_table %>% 
     dplyr::select(cod_pai, codigo) %>% 
     dplyr::filter(!is.na(cod_pai)) %>% 
     dplyr::distinct(cod_pai, codigo) %>% 
     as.matrix() %>% 
     apply(2, as.character) %>% 
-    cria_bn %>% 
+    cria_bn() %>% 
     bnlearn::leaf.nodes()
+}
+
+add_leaf_flag <- function(d) {
+  ife <- dplyr::if_else
+  le <- function(x) dplyr::lead(x == '', default = TRUE)
+  d %>% 
+    dplyr::mutate(
+      folha = ife(n6 != '', 'n6', ''),
+      folha = ife(n5 != '' & le(n6), 'n5', folha),
+      folha = ife(n4 != '' & le(n5), 'n4', folha),
+      folha = ife(n3 != '' & le(n4), 'n3', folha),
+      folha = ife(n2 != '' & le(n3), 'n2', folha),
+      folha = ife(n1 != '' & le(n2), 'n1', folha)
+    )
 }
